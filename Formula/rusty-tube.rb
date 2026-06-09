a29284ef620ac57a1e580e24b0b5a98c1bd2af85
augustofretes <augustofretes@gmail.com>
2026-06-09 18:58:26 -0500

    Bump rusty-tube to 0.1.1

--- Formula/rusty-tube.rb
+++ Formula/rusty-tube.rb
@@ -1,8 +1,8 @@
 class RustyTube < Formula
   desc "Terminal-based YouTube Music player written in Rust"
   homepage "https://github.com/augustofretes/rusty-tube"
-  url "https://github.com/augustofretes/rusty-tube/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "2cff99791148ca9054c53af0400916bcb40046b3bbdf8543629714341eef92f2"
+  url "https://github.com/augustofretes/rusty-tube/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "cb9c860ac6d19ada40fe28c97d68369af40aeec33ca7cfec874bf1fcbeb46e84"
   license "MIT"
   head "https://github.com/augustofretes/rusty-tube.git", branch: "main"
 
