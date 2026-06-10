37e1fb820ad6c8d97799c3b3bdd485b1e8ab0912
augustofretes <augustofretes@gmail.com>
2026-06-09 19:01:39 -0500

    Pin rusty-tube 0.1.1 source commit

--- Formula/rusty-tube.rb
+++ Formula/rusty-tube.rb
@@ -1,8 +1,9 @@
 class RustyTube < Formula
   desc "Terminal-based YouTube Music player written in Rust"
   homepage "https://github.com/augustofretes/rusty-tube"
-  url "https://github.com/augustofretes/rusty-tube/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "cb9c860ac6d19ada40fe28c97d68369af40aeec33ca7cfec874bf1fcbeb46e84"
+  url "https://github.com/augustofretes/rusty-tube/archive/a16811bc8175f2ebd6578e72b671de0053f3af1b.tar.gz"
+  version "0.1.1"
+  sha256 "25680d37056ce8c6bed9ed31065a562dc204180627df9ff2a8c31928b945e1d7"
   license "MIT"
   head "https://github.com/augustofretes/rusty-tube.git", branch: "main"
 
