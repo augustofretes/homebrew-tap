7730e13f16afc8943249bd7fd969707a97fbb8ec
augustofretes <augustofretes@gmail.com>
2026-06-09 20:05:51 -0500

    Use rusty-tube binary on macOS arm64

--- Formula/rusty-tube.rb
+++ Formula/rusty-tube.rb
@@ -1,18 +1,29 @@
 class RustyTube < Formula
   desc "Terminal-based YouTube Music player written in Rust"
   homepage "https://github.com/augustofretes/rusty-tube"
-  url "https://github.com/augustofretes/rusty-tube/archive/refs/tags/v0.1.2.tar.gz"
   version "0.1.2"
-  sha256 "9fb2cbede8aaa2d743d62411bbfc6e14a1c18e26beb8b3d1f3097ba2595115af"
   license "MIT"
   head "https://github.com/augustofretes/rusty-tube.git", branch: "main"
 
-  depends_on "rust" => :build
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/augustofretes/rusty-tube/releases/download/v0.1.2/rusty-tube-v0.1.2-aarch64-apple-darwin.tar.gz"
+    sha256 "0874540edb11dfd8f8e583fb75f3315f3882702332a7f8a9ce09db182ab6b4e7"
+  else
+    url "https://github.com/augustofretes/rusty-tube/archive/refs/tags/v0.1.2.tar.gz"
+    sha256 "9fb2cbede8aaa2d743d62411bbfc6e14a1c18e26beb8b3d1f3097ba2595115af"
+
+    depends_on "rust" => :build
+  end
+
   depends_on "ffmpeg"
   depends_on "yt-dlp"
 
   def install
-    system "cargo", "install", *std_cargo_args
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "rusty-tube"
+    else
+      system "cargo", "install", *std_cargo_args
+    end
   end
 
   test do
