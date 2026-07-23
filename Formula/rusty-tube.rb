9c022771478c5f8f3504f8bc8d3899fd429df3db
augustofretes <augustofretes@gmail.com>
2026-07-23 12:40:46 -0500

    Update rusty-tube to 0.1.3

--- Formula/rusty-tube.rb
+++ Formula/rusty-tube.rb
@@ -1,16 +1,16 @@
 class RustyTube < Formula
   desc "Terminal-based YouTube Music player written in Rust"
   homepage "https://github.com/augustofretes/rusty-tube"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
   head "https://github.com/augustofretes/rusty-tube.git", branch: "main"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/augustofretes/rusty-tube/releases/download/v0.1.2/rusty-tube-v0.1.2-aarch64-apple-darwin.tar.gz"
-    sha256 "0874540edb11dfd8f8e583fb75f3315f3882702332a7f8a9ce09db182ab6b4e7"
+    url "https://github.com/augustofretes/rusty-tube/releases/download/v0.1.3/rusty-tube-v0.1.3-aarch64-apple-darwin.tar.gz"
+    sha256 "9d990600056d736ddd54aa034ec65ee675c62d10b2b8b6057380939a1cc929b7"
   else
-    url "https://github.com/augustofretes/rusty-tube/archive/refs/tags/v0.1.2.tar.gz"
-    sha256 "9fb2cbede8aaa2d743d62411bbfc6e14a1c18e26beb8b3d1f3097ba2595115af"
+    url "https://github.com/augustofretes/rusty-tube/archive/refs/tags/v0.1.3.tar.gz"
+    sha256 "7b3200fd46d2e77bb09a4d4eeea0fe824766dc32b1d22524fbcc4df993130af3"
 
     depends_on "rust" => :build
   end
